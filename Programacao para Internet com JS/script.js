790a811790d9abfd48ae0f114abc3389660f33d9
Douglas Correa Gomes <douglascgom@gmail.com>
2022-06-05 19:13:56 -0300

    Atulizacao do curso

---
+++ Programacao para Internet com JS/script.js
@@ -0,0 +1,101 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar</b> " ;
+    //console.log(document.getElementById("agradecimento"));
+    //alert("Obrigado por clicar")
+}
+ function redirecionar (){
+    //Abre em outro pagina
+    window.open("https://web.dio.me/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d?back=/browse");
+    
+    /* Abre na msm pagina
+    window.location.href = "https://web.dio.me/course/programacao-para-internet-com-javascript/learning/ddb54ad6-55fb-4eec-90d5-561a6010d14d?back=/browse" */
+    }
+
+
+
+
+
+
+
+
+
+
+
+/*
+function soma(n1 , n2){
+    return n1 + n2
+}
+
+var validar = 0
+function validaIdade(idade){
+    validar;   
+    if(idade >= 18){
+        validar = true
+    } else {
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade ?");
+console.log (validaIdade(idade))
+
+
+
+
+
+var d = new Date()
+console.log(d.getHours())
+console.log(d.getMinutes())
+ 
+
+
+
+var numeros;
+for(numeros = 1 ; numeros <= 5 ; numeros++){
+    console.log(numeros)
+}
+
+
+
+var numeros = 0
+while(numeros <= 5){
+    console.log(numeros);
+        numeros++
+}
+
+
+var idade = prompt("Qual é a sua idade ?")
+ if (idade >= 18){
+     alert("Maior de idade")
+ } else {
+     alert("Menor de Idade")
+ }
+
+var lista = [{nome:"Douglas", cor: "Branco", idade: 30, cidade: "Timoteo"}]
+console.log(lista)
+alert(lista[0].nome)
+
+var lista = {nome:"Douglas", cor: "Branco", idade: 30, cidade: "Timoteo"}
+console.log(lista.nome)
+
+
+var lista = ["uva", "pera", "melancia"];
+lista.push("banana")
+lista.pop();
+ 
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
+var nome = "Douglas Correa Gomes"
+var idade = 30
+var idade2 = 10
+var frase = "Inglaterra e  o melhor pais do mundo"
+alert(nome + ' tem ' + idade  + ' anos ')
+alert(idade + idade2)
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase);
+alert(frase.replace('Inglaterra','Brasil'));
+*/
