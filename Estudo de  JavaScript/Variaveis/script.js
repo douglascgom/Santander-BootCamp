4de7a1c5323b9b844026cabb246e254d6013cf82
Douglas Correa Gomes <douglascgom@gmail.com>
2022-06-11 19:20:57 -0300

    Estudo de Variaveis / JS

---
+++ Estudo de  JavaScript/Variaveis/script.js
@@ -0,0 +1,22 @@
+
+var firstName = 'Douglas';
+let lastName = 'Gomes';
+
+if(firstName ==='Douglas'){
+        var firstName = 'Pedro';
+    let lastName =  'Correa';  
+    
+}
+
+console.log(firstName, lastName);
+
+
+/*
+const FIRST_NAME = "Douglas"
+
+
+console.log(FIRST_NAME)
+ 
+*/
+
+
